7c9e7a942a7d23aa2ddffedda4f0d99a08b4afe8
Samuel Pineda <samuel@creamos.co>
2026-05-04 19:30:18 -0500

    chore: remove unused file configurations and associated references

--- src/config/env.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  API_URL: import.meta.env.VITE_API_URL,
-  APP_NAME: import.meta.env.VITE_APP_NAME || 'Elite',
-  APP_VERSION: import.meta.env.VITE_APP_VERSION || '1.0.0',
-};
